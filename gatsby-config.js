b5a5fa21ad784f1e7027ca7605c942ba811d1dfb
usdonoso <usdonoso@uc.cl>
2021-08-05 00:05:14 -0400

    hola

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,25 @@
+const config = require('./config');
+
 module.exports = {
+  pathPrefix: config.pathPrefix,
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "mipagina",
+    title: config.siteTitle,
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.manifestName,
+        short_name: config.manifestShortName,
+        start_url: config.pathPrefix || config.manifestStartUrl,
+        background_color: config.manifestBackgroundColor,
+        theme_color: config.manifestThemeColor,
+        display: config.manifestDisplay,
+        icon: config.manifestIcon, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+  ],
 };
